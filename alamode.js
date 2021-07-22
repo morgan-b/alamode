03be02309d1e116730c75586fdfbedcbe37704b9
Morgan Bailey <mabailey079@Gmail.com>
2021-07-22 12:42:49 -0400

    adding video element

--- alamode.js
+++ alamode.js
@@ -417,6 +417,56 @@ var alamode = {
       })
     }
   },
+  addVideosToTables: function(o) {
+
+    var tableId = "#" + o["table"],
+        videoColumn = o["column"],
+        videoHeight = o["image_height"] || 100;
+
+    setTimeout(function() {
+      drawVideos();
+    },1000)
+
+    $(tableId).keyup(function() {
+      setTimeout(function() {
+        drawVideos();
+      },500)
+    });
+
+    $(tableId).mousemove(function() {
+      drawVideos();
+    })
+
+    function drawVideos() {
+      var tableDiv = $(tableId + " table"),
+          tableHeader = $(tableId + " .js-header-table"),
+          headers = !tableHeader ? $(tableHeader).find("th") : $(tableId + " .js-col-header"),
+          rows = tableDiv.find("tr"),
+          columnIndex = 0;
+
+      headers.each(function() {
+        text = $(this).find(".axel-table-header-label").text()
+        if (text == videoColumn) {
+          columnIndex = +$(this).attr("data-axel-column")
+        }
+      })
+
+      rows.each(function() {
+        var cells = $(this).find("td");
+
+        cells.each(function(i) {
+          if (i == (columnIndex - 1)) {
+            var content = $(this).text();
+
+            if ($(this).find("video").length == 0) {
+              $(this).css("text-align","center")
+              $(this).html("<video style='height: " + videoHeight + "px;' src='" + content + "' type='video/mp4''>")
+            }
+          }
+        })
+      })
+    }
+  },
 
   resizeChartHeight: function(o) {
     var chart = o["chart"],
