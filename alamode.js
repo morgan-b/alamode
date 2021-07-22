75806b06cb743de1f33ebb638a9cfc8ebd2c8a95
Morgan Bailey <mabailey079@Gmail.com>
2021-07-22 14:05:03 -0400

    changing video tag for mode

--- alamode.js
+++ alamode.js
@@ -460,8 +460,7 @@ var alamode2 = {
 
             if ($(this).find("video").length == 0) {
               $(this).css("text-align","center")
-              $(this).html("<video height='240' width='320'>" 
-              + "<source src='" + content + "' type='video/mp4'></video>")
+              $(this).html("<video height='240' width='320' src='" + content + "' type='video/mp4'></video>")
             }
           }
         })
